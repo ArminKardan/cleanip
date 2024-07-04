66405300da00e19abbf2bc3378052c7bc5a97c5a
arminkardan <$armin.fire@gmail.com>
2024-07-04 14:16:58 +0330

    Update

--- run.sh
+++ run.sh
@@ -3,15 +3,7 @@ output_script="/etc/cleanip/cleanip.sh"
 
 # Use a here document to write the content to the new shell script
 cat << 'EOF' > $output_script
-#!/bin/bash
-
-URL="https://download.docker.com/linux/ubuntu/gpg"
-STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" "$URL")
-if [ "$STATUS" -eq 200 ]; then
-  echo "Hooray! Your Network IP is clean (Status: $STATUS)."
-else
-  echo "Network is dirty (Status: $STATUS)."
-fi
+hi
 EOF
 
 # Make the new script executable
@@ -22,7 +14,7 @@ chmod +x $output_script
 
 
 
-NEW_PATH=$(realpath "/etc/cleanip/cleanip.sh")
+NEW_PATH="/etc/cleanip/cleanip.sh"
 # Check if the path is already in the PATH
 if [[ ":$PATH:" == *":$NEW_PATH:"* ]]; then
   echo "The path $NEW_PATH is already in the PATH."
